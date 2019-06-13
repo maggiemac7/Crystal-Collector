03d0e9e579b306ce0ff0df1a87afa81ac5f327f3
Maggie MacVey <maggiemacvey7@gmail.com>
2019-06-13 13:59:33 -0400

    html file complete.  css and reset files linked. css non responsive

---
+++ javascript/game.js
@@ -0,0 +1,32 @@
+// Random Number between 19-120 id="number-to-guess"
+
+
+// When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+// Red Crystal Random number between 1-12 OnClick id="red"-->
+// Blue Crystal Random number between 1-12 OnClick id="blue"
+// Orange Crystal Random number between 1-12 OnClick id="orange"
+// Green Crystal Random number between 1-12 OnClick id="green"
+
+// Your game will hide this amount until the player clicks a crystal.
+
+// When they do click one, update the player's score counter.
+// Score counter adding together value of crystals clicked
+
+// The player wins if their total score matches the random number from the beginning of the game.
+// Score +1 if score counter === random number
+
+// Game resets if score counter === random numbers
+
+// The player loses if their score goes above the random number.
+// Loss score +1 is score counter > random number
+
+// Game resets if score counter > random number
+
+// The game restarts whenever the player wins or loses.
+// Game reset generates all new random numbers
+// Game reset resets score counter to 0
+
+// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+
+// The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
