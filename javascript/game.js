fd50432a2100d1c02de34cd352ca6b6d9ee66e01
Maggie MacVey <maggiemacvey7@gmail.com>
2019-06-20 14:07:42 -0400

    As done as its going to get before it's graded probably

--- javascript/game.js
+++ javascript/game.js
@@ -6,61 +6,86 @@ $(document).ready(function() {
 // Random Number between 19-120 id="number-to-guess"
 var computerPick = Math.floor(Math.random() * (121 - 19) + 19);
 console.log("Computer Pick: " + computerPick);
+$("#number-to-guess").html(computerPick);
 
 // score board variables
 var wins = 0;
 console.log("Wins: " + wins);
+$(".gamesWon").html("Wins: " + wins);
 
 var losses = 0;
 console.log("Losses: " + losses);
+$(".gamesLost").html("Losses: " + losses);
 
 var crystalTotal = 0;
 console.log("Crystal Total: " + crystalTotal);
+$(".crystal-total").html("Crystal Total: " + crystalTotal);
 
 // Red Crystal Random number between 1-12 OnClick id="red"-->
 var redGem = Math.floor(Math.random() * 12)+1; 
-$("#red").html("<img src=" + "images/red-crystal.jpg" + " value=" + redGem + ">");
 console.log("Red Crystal: " + redGem);
 
 // Blue Crystal Random number between 1-12 OnClick id="blue"
 var blueGem = Math.floor(Math.random() * 12)+1; 
-$("#blue").html("<img src=" + "images/blue-crystal.jpg" + " value=" + redGem + ">");
 console.log("Blue Crystal: " + blueGem);
 
 // Orange Crystal Random number between 1-12 OnClick id="orange"
 var orangeGem = Math.floor(Math.random() * 12)+1; 
-$("#orange").html("<img src=" + "images/orange-crystal.jpg" + " value=" + redGem + ">");
 console.log("Orange Crystal: " + orangeGem);
 
 // Green Crystal Random number between 1-12 OnClick id="green"
 var greenGem = Math.floor(Math.random() * 12)+1; 
-$("#green").html("<img src=" + "images/green-crystal.jpg" + " value=" + redGem + ">");
 console.log("Green Crystal: " + greenGem);
 
 
 $("#red").click(function(){
+    $("#red").html("<img src=" + "images/red-crystal.jpg" + " value=" + redGem + ">");
+    console.log("Red Crystal: " + redGem);
     CrystalClicked(this);
     console.log(this);
     
 });
 $("#blue").click(function(){
+    $("#blue").html("<img src=" + "images/blue-crystal.jpg" + " value=" + blueGem + ">");
+    console.log("Blue Crystal: " + blueGem);
     CrystalClicked(this);
     console.log(this);
 });
 $("#orange").click(function(){
+    $("#orange").html("<img src=" + "images/orange-crystal.jpg" + " value=" + orangeGem + ">");
+    console.log("Orange Crystal: " + orangeGem);
     CrystalClicked(this);
     console.log(this);
 });
 $("#green").click(function(){
+    $("#green").html("<img src=" + "images/green-crystal.jpg" + " value=" + greenGem + ">");
+    console.log("Green Crystal: " + greenGem);
     CrystalClicked(this);
     console.log(this);
 });
 
-function CrystalClicked (Crystal){
-    console.log($(Crystal).attr("id"));
+
+//UGGGGGGGH. I GIVE UP. I DON'T KNOW HOW TO LINK THE CLICKED CRYSTAL TO THE TOTAL VALUE. THE INTERNET DOESN'T KNOW EITHER THOUGH.. SO THAT MAKES ME FEEL BETTER. 
+
+var imageCrystal = $("<value=>");
+
+imageCrystal.addClass("crystal-image");
+
+$(".crystals").append(imageCrystal);
+
+$(".crystal-image").on("click", function() {
+
+    var newTotal = ($(this).attr("=value"));
+    newTotal = parseInt(newTotal);
+
+    crystalTotal += newTotal;
+
+
+// function CrystalClicked (Crystal){
+//     console.log($(Crystal).attr("id"));
     
-    var ClickedCrystal = $(Crystal).attr("id")
-}
+//     var ClickedCrystal = $(Crystal).attr("id")
+});
 
 function reset () {
     computerPick = Math.floor(Math.random() * (121 - 19) + 19); 
@@ -86,10 +111,10 @@ function reset () {
     $("#green").html("<img src=" + "images/green-crystal.jpg" + " value=" + greenGem + ">");
     console.log("Green Crystal: " + greenGem);
 // ------------------------------------------------------
-    $(".crystals").on("click", function () {
-    var newTotal = crystalTotal += parseInt($(this).attr("value")); 
-        console.log("New Score: " + newTotal); 
-    $(".crystal-total").html(newTotal); 
+    $("#red").on("click", function () {
+    var newTotal = crystalTotal += parseInt($(this).attr("value=")); 
+        console.log("New Total: " + newTotal); 
+    $(".crystal-total").html("Crystal Total: " + newTotal); 
 
     if(newTotal === computerPick) { 
         wins++ ; 
